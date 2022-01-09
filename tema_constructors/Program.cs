5ea41c904710c95a0cf71255cb5e709c5185ae3b
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-01-09 23:32:32 +0200

    Prints Rectangle with constructor overloading

--- tema_constructors/Program.cs
+++ tema_constructors/Program.cs
@@ -16,8 +16,14 @@ namespace tema_constructors
             Triangle triangle = new Triangle(4, 8, 6, 2, 7, 10);
             Console.WriteLine("The points of the triangle");
             triangle.PrintTriangle();
+            Console.WriteLine("New location of triangle");
             triangle.MoveTriangle(110, 150, 78, 90, 68, 13);
             triangle.PrintTriangle();
+            Console.WriteLine();
+
+            Console.WriteLine("***Rectangle***");
+            Rectangle rectangle = new Rectangle(7, 7, 11, 9, "Usless");
+            rectangle.PrintRectangle();
         }
     }
 }
---
+++ tema_constructors/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema_constructors
+{
+    class Rectangle
+    {
+        int _point1X { get; set; }
+        int _point1Y { get; set; }
+        int _point2X { get; set; }
+        int _point2Y { get; set; }
+        int _point3X { get; set; }
+        int _point3Y { get; set; }
+        int _point4X { get; set; }
+        int _point4Y { get; set; }
+
+        public Rectangle(int point1x,int point1y, int side1, int side2)
+        {
+            this._point1X = point1x;
+            this._point1Y = point1y;
+            this._point2X = _point1X + side1;
+            this._point2Y = _point1Y;
+            this._point3X = _point1X;
+            this._point3Y = _point1Y + side2;
+            this._point4X = _point2X;
+            this._point4Y = _point3Y;
+        }
+
+        public Rectangle(int point1x, int point1y, int point4x, int point4y, string useless)
+        {
+            this._point1X = point1x;
+            this._point1Y = point1y;
+            this._point4X = point4x;
+            this._point4Y = point4y;
+            this._point2X = _point4X;
+            this._point2Y = _point1Y;
+            this._point3X = _point1X;
+            this._point3Y = _point4Y;
+        }
+
+        public void PrintRectangle()
+        {
+            Console.WriteLine($"Point1 = X({this._point1X}), Y({this._point1Y})");
+            Console.WriteLine($"Point2 = X({this._point2X}), Y({this._point2Y})");
+            Console.WriteLine($"Point3 = X({this._point3X}), Y({this._point3Y})");
+            Console.WriteLine($"Point4 = X({this._point4X}), Y({this._point4Y})");
+            Console.WriteLine($"Side 1 = {this._point2X - this._point1X}");
+            Console.WriteLine($"Side 2 = {this._point3Y - this._point1Y}");
+        }
+
+
+    }
+}
